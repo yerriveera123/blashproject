4e1af8a79e8576e1a25dd8077a62f3a4cd2d6fd9
yerriveera123 <yerriveera@.com>
2024-10-09 14:56:05 +0530

    first commit

--- Blash/settings.py
+++ Blash/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import os
-TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +57,7 @@ ROOT_URLCONF = 'Blash.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
